18a49631a215f37a87419ebbbf5e5a8d0cf8cc19
Tobias Frech <tobias@frech.info>
2021-01-23 20:11:39 +0100

    refactored

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,25 @@
 <template>
   <div id="app">
-    <h1>Jitsi Mod-e-rator</h1>
+    <h1>Jitsi Mod-era-tor</h1>
   </div>
 </template>
 
 <script>
-import JitsiMeetJS from '@lyno/lib-jitsi-meet';
-import $ from 'jquery';
-window.$ = $;
 
-import options from './options/config';
-
-JitsiMeetJS.init();
-JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.INFO);
-
-var connection = new JitsiMeetJS.JitsiConnection(null, null, options);
-
-function onConnectionSuccess() {
-  console.log("onConnectionSuccess", arguments);
-  const room = connection.initJitsiConference("vueconf", {});
-  room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
-  room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, () => {
-    console.log("onConferenceJoined", arguments);
-/*
-    JitsiMeetJS.createLocalTracks({
-      devices: ['video', 'audio']
-    }).then((tracks) => {
-      console.log("onLocalTracks", tracks);
-      tracks.forEach(track => {
-        room.addTrack(track);
-      })
-    }).catch(error => {
-          console.error("There was an error creating the local tracks:", error);
-        }
-    );
-*/
-  });
-  room.join();
-}
-function onConnectionFailed() {
-  console.log("onConnectionFailed", arguments);
-}
-function disconnect() {
-  console.log("disconnect", arguments);
-}
-
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
-
-connection.connect();
-
-function onRemoteTrack() {
-  console.log("onRemoteTrack", arguments);
-}
+import { connect, createAndJoinRoom/*, createTracksAndAddToRoom*/ } from './utils/jitsiUtils.js'
 
 export default {
   name: 'App',
+
+  mounted() {
+    connect().then(connection => {
+      return createAndJoinRoom(connection, 'vueconf');
+    })
+        /*.then(room => createTracksAndAddToRoom(room))
+        .catch(error => console.error(error))*/;
+  }
 }
+
 </script>
 
 <style>
